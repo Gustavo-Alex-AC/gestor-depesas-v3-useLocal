2e8fb313836a9e78a760a98b9414cef5d41e2b16
Gustavo Chitangua <gchitangua@gmail.com>
2024-02-15 14:33:59 +0100

    optimizedpages

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,78 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Income from "./Income";
-import Expense from "./Expense";
-import { DataProvider } from "../contexts/DataContext";
-import Instruções from "./Instruções";
-import Apoio from "./Apoio";
-import Dashboard from "./Dashboard";
-import Welcome from "../pages/auth/Welcome";
-import Protected from "../pages/auth/Protected";
-import PageNotFound from "../pages/auth/PageNotFound";
 
-//import SignIn from "../pages/auth/SignIn";
+import { DataProvider } from "../contexts/DataContext";
+import Protected from "../pages/auth/Protected";
+import { lazy } from "react";
+import { Suspense } from "react";
+import SpinnerFullPage from "./SpinnerFullPage";
+
+// import Welcome from "../pages/auth/Welcome";
+// import Dashboard from "./Dashboard";
+// import Income from "./Income";
+// import Expense from "./Expense";
+// import Instruções from "./Instruções";
+// import Apoio from "./Apoio";
+// import PageNotFound from "../pages/auth/PageNotFound";
+
+const Welcome = lazy(() => import("../pages/auth/Welcome"));
+const Dashboard = lazy(() => import("./Dashboard"));
+const Income = lazy(() => import("./Income"));
+const Expense = lazy(() => import("./Expense"));
+const Instruções = lazy(() => import("./Instruções"));
+const Apoio = lazy(() => import("./Apoio"));
+const PageNotFound = lazy(() => import("../pages/auth/PageNotFound"));
 
 function App() {
   return (
     <div>
       <DataProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route
-              path="/dashboard"
-              element={
-                <Protected>
-                  <Dashboard />
-                </Protected>
-              }
-            />
-            <Route
-              path="receitas"
-              element={
-                <Protected>
-                  <Income />
-                </Protected>
-              }
-            />
-            <Route
-              path="despesas"
-              element={
-                <Protected>
-                  <Expense />
-                </Protected>
-              }
-            />
-            <Route
-              path="instrucoes"
-              element={
-                <Protected>
-                  <Instruções />
-                </Protected>
-              }
-            />
-            <Route
-              path="apoio"
-              element={
-                <Protected>
-                  <Apoio />
-                </Protected>
-              }
-            />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route path="/" element={<Welcome />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <Protected>
+                    <Dashboard />
+                  </Protected>
+                }
+              />
+              <Route
+                path="receitas"
+                element={
+                  <Protected>
+                    <Income />
+                  </Protected>
+                }
+              />
+              <Route
+                path="despesas"
+                element={
+                  <Protected>
+                    <Expense />
+                  </Protected>
+                }
+              />
+              <Route
+                path="instrucoes"
+                element={
+                  <Protected>
+                    <Instruções />
+                  </Protected>
+                }
+              />
+              <Route
+                path="apoio"
+                element={
+                  <Protected>
+                    <Apoio />
+                  </Protected>
+                }
+              />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </DataProvider>
     </div>
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,25 +1,11 @@
-import React from "react";
-import { css } from "@emotion/react";
-import { BeatLoader } from "react-spinners";
+import styles from "./Spinner.module.css";
 
-const override = css`
-  display: block;
-  margin: 0 auto;
-  margin-left: 3rem;
-`;
-
-const styleSpinner = {
-  // display:"block",
-  margin: "0 auto",
-  marginLeft: "3rem",
-}
-
-const Spinner = () => {
+function Spinner() {
   return (
-    <div className={styleSpinner}>
-      <BeatLoader css={override} size={15} color={"#36D7B7"} loading={true} />
+    <div className={styles.spinnerContainer}>
+      <div className={styles.spinner}></div>
     </div>
   );
-};
+}
 
 export default Spinner;
